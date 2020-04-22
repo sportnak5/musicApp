7dc9d0c3e59f0e8b1d53f7713ca50829f0756932
pspaffordOCV <pspafford@myocv.com>
2020-04-21 21:13:36 -0500

    Styles headers to list views

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { IconButton } from '@material-ui/core'
 import { AddCircle } from '@material-ui/icons'
+import styles from './Header.module.css'
 
 export default class Header extends React.PureComponent {
     render() {
         return(
-            <span>
+            <span style={{backgroundColor: 'white', padding: '10px', borderRadius: '4px'}}>
                 {this.props.page === 'Library' ? 
                     'Library'
                 : this.props.page === 'Playlists' ?
