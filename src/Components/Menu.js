170a83dbcef99b439385d0706598bccde75d8a5e
pspaffordOCV <pspafford@myocv.com>
2020-04-21 21:03:03 -0500

    Color of menu buttons to white

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import styles from './Menu.module.css'
-import { Button, Icon } from '@material-ui/core'
+import { Button, Icon, withStyles } from '@material-ui/core'
 import {MusicNote, Menu, Search, QueueMusic } from '@material-ui/icons'
+
+const StyledButton = withStyles({
+    root: {
+        backgroundColor: 'white'
+    },
+    label: {
+        backgroundColor: 'white'
+    }
+})(Button);
 export default class extends React.PureComponent {
     render() {
         const menuItems = ['Playlists', 'My Queue', 'Search', 'Library'];
@@ -9,10 +18,10 @@ export default class extends React.PureComponent {
         return menuItems.map((item, i=0) => {
             return(
                 <li className = {styles.list} key = {item}>
-                    <Button variant="outlined" className = {styles.menuButton} onClick = {() => this.props.pageChanger(item)}>
+                    <StyledButton variant="outlined" className = {styles.menuButton} onClick = {() => this.props.pageChanger(item)}>
                     {menuIcons[i]}
                        <h2 style={{paddingLeft: '20px'}}>{item}</h2>
-                    </Button>
+                    </StyledButton>
                 </li>
             )
         })
